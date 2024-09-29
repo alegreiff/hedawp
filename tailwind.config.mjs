91eaf102cc405950983bff75bb457aee12f1ecf5
Jaime de Greiff <alegreiff@gmail.com>
2024-09-28 20:39:16 -0500

    parte 2

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,13 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		fontFamily: {
+			titulos: ['Assistant Variable', 'sans-serif'],
+			onest: ['Onest Variable', 'sans-serif'],
+		},
+		extend: {
+			
+		},
 	},
 	plugins: [
 		require('@tailwindcss/typography'),     
